064cad83217710078c2e28ea843ca1f615b7776c
Sudheesh B <sudheesh.b@codelynks.com>
2025-05-26 14:19:16 +0530

    Code cleanup.

--- app/src/main/java/com/sb/clickcounter/ui/view/CounterView.kt
+++ app/src/main/java/com/sb/clickcounter/ui/view/CounterView.kt
@@ -35,6 +35,11 @@ import com.sb.clickcounter.R
 @Composable
 fun CounterView(resources: Resources, innerPadding: PaddingValues) {
     var count by rememberSaveable { mutableIntStateOf(0) }
+
+    val plusClick = { if (count < 9999) count++ }
+    val minusClick = { if (count > 0) count-- }
+    val resetClick = { count = 0 }
+
     BoxWithConstraints {
         if (maxWidth < 600.dp) {
             Column(
@@ -44,89 +49,90 @@ fun CounterView(resources: Resources, innerPadding: PaddingValues) {
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Spacer(Modifier.fillMaxHeight(0.2f))
-                Text(
-                    count.toString(),
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 100.sp,
-                    maxLines = 1,
-                    minLines = 1
-                )
+                CounterText(count)
                 Spacer(Modifier.fillMaxHeight(0.3f))
-                Row {
-                    ElevatedButton(modifier = Modifier.size(100.dp), onClick = { if (count > 0) count-- }) {
-                        Text(
-                            resources.getString(R.string.minus),
-                            fontSize = 50.sp,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                    Spacer(Modifier.width(50.dp))
-                    ElevatedButton(
-                        modifier = Modifier.size(100.dp),
-                        onClick = { if (count < 9999) count++ }) {
-                        Text(
-                            resources.getString(R.string.plus),
-                            fontSize = 50.sp,
-                            color = MaterialTheme.colorScheme.onBackground
-                        )
-                    }
-                }
-                TextButton(
-                    modifier = Modifier.padding(top = 32.dp),
-                    onClick = { count = 0 }
-                ) {
-                    Text(resources.getString(R.string.reset), fontSize = 16.sp, color = MaterialTheme.colorScheme.onBackground)
-                }
+                ControlView(resources, plusClick, minusClick, resetClick)
             }
         } else {
             Row(
                 modifier = Modifier
-                    .fillMaxSize().padding(horizontal = innerPadding.calculateStartPadding(
-                        LayoutDirection.Ltr)),
+                    .fillMaxSize()
+                    .padding(
+                        horizontal = innerPadding.calculateStartPadding(
+                            LayoutDirection.Ltr
+                        )
+                    ),
                 verticalAlignment = Alignment.CenterVertically,
             ) {
-                Text(
-                    count.toString(),
-                    modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(0.45f),
-                    fontWeight = FontWeight.Bold,
-                    fontSize = 100.sp,
-                    textAlign = TextAlign.Center,
-                    maxLines = 1,
-                    minLines = 1
-                )
-                Column ( modifier = Modifier.padding(horizontal = 16.dp).fillMaxWidth(1f).fillMaxHeight(1f),
+                CounterText(count, Modifier.fillMaxWidth(0.45f))
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 16.dp)
+                        .fillMaxWidth(1f)
+                        .fillMaxHeight(1f),
                     horizontalAlignment = Alignment.CenterHorizontally,
                     verticalArrangement = Arrangement.Center
-                ){
-                    Row {
-                        ElevatedButton(modifier = Modifier.size(100.dp), onClick = { if (count > 0) count-- }) {
-                            Text(
-                                resources.getString(R.string.minus),
-                                fontSize = 50.sp,
-                                color = MaterialTheme.colorScheme.onBackground
-                            )
-                        }
-                        Spacer(Modifier.width(50.dp))
-                        ElevatedButton(
-                            modifier = Modifier.size(100.dp),
-                            onClick = { if (count < 9999) count++ }) {
-                            Text(
-                                resources.getString(R.string.plus),
-                                fontSize = 50.sp,
-                                color = MaterialTheme.colorScheme.onBackground
-                            )
-                        }
-                    }
-                    TextButton(
-                        modifier = Modifier.padding(top = 32.dp),
-                        onClick = { count = 0 }
-                    ) {
-                        Text(resources.getString(R.string.reset), fontSize = 16.sp, color = MaterialTheme.colorScheme.onBackground)
-                    }
+                ) {
+                    ControlView(resources, plusClick, minusClick, resetClick)
                 }
 
             }
+        }
+    }
+}
+
+@Composable
+fun CounterText(count: Int, modifier: Modifier = Modifier) {
+    Text(
+        count.toString(),
+        modifier = modifier.padding(horizontal = 16.dp),
+        fontWeight = FontWeight.Bold,
+        fontSize = 100.sp,
+        textAlign = TextAlign.Center,
+        maxLines = 1
+    )
+}
+
+@Composable
+fun ControlView(
+    resources: Resources,
+    onPlusClick: () -> Unit,
+    onMinusClick: () -> Unit,
+    onResetClick: () -> Unit,
+) {
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Row {
+            ElevatedButton(
+                modifier = Modifier.size(100.dp),
+                onClick = onMinusClick
+            ) {
+                Text(
+                    resources.getString(R.string.minus),
+                    fontSize = 50.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+            Spacer(Modifier.width(50.dp))
+            ElevatedButton(
+                modifier = Modifier.size(100.dp),
+                onClick = onPlusClick
+            ) {
+                Text(
+                    resources.getString(R.string.plus),
+                    fontSize = 50.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+            }
+        }
+        TextButton(
+            modifier = Modifier.padding(top = 32.dp),
+            onClick = onResetClick
+        ) {
+            Text(
+                resources.getString(R.string.reset),
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onBackground
+            )
         }
     }
 }
